787623c85c5938f781997225c3400987e83720ce
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2022-10-08 17:02:31 +0200

    Copy second version of the Gradio UI.

--- src/gradio_app.py
+++ src/gradio_app.py
@@ -2,29 +2,23 @@ import gradio as gr
 
 
 def choosistant(review_text, procon="Benefits"):
-    if procon == "Benefits":
-        return review_text[0:4]
-    elif procon == "Drawbacks":
-        return review_text[-4:]
-    else:
-        return "Select whether to see either benefits or drawbacks of this product."
+    # waiting for model
+    return "none yet" + " " + review_text
 
 
 text_input = gr.Textbox(
     lines=8, placeholder="Enter the review text here...", label="Review Text"
 )
 new_title = gr.Textbox(label="Detected pro/con")
+example_url = "http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Musical_Instruments_5.json.gz"
 iface = gr.Interface(
     choosistant,
-    [text_input, gr.Radio(["Benefits", "Drawbacks"], label="Pros/Cons")],
+    [text_input],
     new_title,
-    description="Checkout some amazon reviews eg at [http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Musical_Instruments_5.json.gz](http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Musical_Instruments_5.json.gz)",
+    description=f"Checkout some amazon reviews eg at [{example_url}]({example_url})",
     allow_flagging="manual",
-    flagging_options=["Incorrect", "Insufficient", "Offensive", "Other"],
-    examples=[
-        ["The top side came ripped off", "Drawbacks"],
-        ["The gear is child friendly", "Benefits"],
-    ],
+    flagging_options=["Incorrect", "Offensive", "Other"],
+    examples=[["The top side came ripped off"], ["The gear is child friendly"]],
 )
 
 iface.launch(server_port=8090)
